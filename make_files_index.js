54f32a6bd7f8414f62e17cad2920f1e48dfd69c8
John Chadwick <john@jchw.io>
2021-04-12 19:10:40 -0700

    Static files update.

--- make_files_index.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs')
-const pug = require('pug');
-const files = fs.readdirSync('./static/files').filter(x => x !== 'index.html');
-
-fs.writeFileSync('./static/files/index.html', pug.renderFile('./files.pug', { files }));
